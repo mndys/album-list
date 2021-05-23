a1b5b63872413f555b760a88076c2558b889420e
Mandy Schwarze <mandy2301@me.com>
2021-05-23 19:52:18 +0200

    Add fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react"
 import "./App.css"
 
 function App() {
-  return <div className="App"></div>
+  const [isLoading, setIsLoading] = useState(null)
+  const [error, setError] = useState(null)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const url = "https://hk-test-api.herokuapp.com/albums"
+    const headers = {
+      headers: { "X-API-Key": "secret", Accept: "application/json" },
+    }
+    fetch(url, headers)
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        }
+        throw res
+      })
+      .then((res) => setData(res))
+      .catch((err) => console.error("There was an error fetching data", err))
+  }, [])
+
+  return (
+    <div className="App">
+      {data === null && "is Loading..."}
+      {data !== null && data.map((element) => console.log(element))}
+    </div>
+  )
 }
 
 export default App
