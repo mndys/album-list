053c590a93d60a12f9df21ba4679badeb073dd7e
Mandy Schwarze <mandy2301@me.com>
2021-05-23 22:44:35 +0200

    Add error handling and move api key to env variable

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
 import { useEffect, useState } from "react"
-import "./App.css"
 
 function App() {
-  const [isLoading, setIsLoading] = useState(null)
   const [error, setError] = useState(null)
   const [data, setData] = useState(null)
 
   useEffect(() => {
     const url = "https://hk-test-api.herokuapp.com/albums"
     const headers = {
-      headers: { "X-API-Key": "secret", Accept: "application/json" },
+      headers: {
+        "X-API-Key": process.env.REACT_APP_X_API_KEY,
+        Accept: "application/json",
+      },
     }
     fetch(url, headers)
-      .then((res) => {
-        if (res.ok) {
-          return res.json()
-        }
-        throw res
-      })
+      .then((res) => res.json())
       .then((res) => setData(res))
-      .catch((err) => console.error("There was an error fetching data", err))
+      .catch((err) => {
+        setError(err)
+        console.error("There was an error fetching data", err)
+      })
   }, [])
 
   return (
     <div className="App">
-      {data === null && "is Loading..."}
-      {data !== null && data.map((element) => console.log(element))}
+      {data === null && error && "Oh no! There was an error loading your data."}
+      {data === null && !error && "is Loading..."}
+      {data !== null && (
+        <ul>
+          {data.map(({ id, title, artist, released_in }) => (
+            <li key={id}>
+              <h1>{title}</h1>
+              <span>{artist}</span>
+              <span>({released_in})</span>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
