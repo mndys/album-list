e1d59e9490bf4ac1a8a592e7dd38e9fcdef94819
Mandy Schwarze <mandy2301@me.com>
2021-05-26 14:05:51 +0200

    Add and style backdrop, exchange flex for grid

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -11,56 +11,67 @@ export default function Modal({ status, id, handleEdit, handleClose }) {
   useClickedOutside(modal, handleClose)
 
   return (
-    <Content ref={modal}>
-      <h2>Edit Album</h2>
-      <label htmlFor="album">Album</label>
-      <input
-        type="text"
-        name="album"
-        value={newObject.title}
-        onChange={event => {
-          setNewObject({ ...newObject, title: event.target.value })
-          console.log(newObject)
-        }}
-      />
-      <label htmlFor="artist">Artist</label>
-      <input
-        type="text"
-        name="artist"
-        value={newObject.artist}
-        onChange={event =>
-          setNewObject({ ...newObject, artist: event.target.value })
-        }
-      />
-      <label htmlFor="year">Year</label>
-      <input
-        type="number"
-        minLength={3}
-        max={new Date().getFullYear() + 2}
-        name="year"
-        value={newObject.released_in}
-        onChange={event =>
-          setNewObject({ ...newObject, released_in: event.target.value })
-        }
-      />
-      <button className="cancel" onClick={handleClose}>
-        Cancel
-      </button>
-      <button className="save" onClick={() => handleEdit(id, newObject)}>
-        Save
-      </button>
-    </Content>
+    <Backdrop>
+      <Content ref={modal}>
+        <h2>Edit Album</h2>
+        <label htmlFor="album">Album</label>
+        <input
+          type="text"
+          name="album"
+          value={newObject.title}
+          onChange={event => {
+            setNewObject({ ...newObject, title: event.target.value })
+            console.log(newObject)
+          }}
+        />
+        <label htmlFor="artist">Artist</label>
+        <input
+          type="text"
+          name="artist"
+          value={newObject.artist}
+          onChange={event =>
+            setNewObject({ ...newObject, artist: event.target.value })
+          }
+        />
+        <label htmlFor="year">Year</label>
+        <input
+          type="number"
+          minLength={3}
+          max={new Date().getFullYear() + 2}
+          name="year"
+          value={newObject.released_in}
+          onChange={event =>
+            setNewObject({ ...newObject, released_in: event.target.value })
+          }
+        />
+        <button className="cancel" onClick={handleClose}>
+          Cancel
+        </button>
+        <button className="save" onClick={() => handleEdit(id, newObject)}>
+          Save
+        </button>
+      </Content>
+    </Backdrop>
   )
 }
+
+const Backdrop = styled.section`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: var(--color-overlay);
+`
 
 const Content = styled.div`
   position: absolute;
   top: 20%;
   left: 50%;
   transform: translate(-50%, -20%);
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: auto;
   align-items: center;
   gap: 1em 0.5em;
   width: clamp(300px, 90vw, 500px);
@@ -68,22 +79,24 @@ const Content = styled.div`
   border: 1px solid #000;
   padding: 2em;
   background-color: var(--color-platinum);
+  box-shadow: var(--shadow);
 
   h2 {
-    flex: 100%;
+    grid-column: 1 / -1;
     text-align: center;
-    padding-bottom: 1em;
+    padding-bottom: 0.5em;
   }
 
   label {
-    flex: 15%;
+    grid-column: 1 / 2;
   }
+
   input {
-    flex: 80%;
+    grid-column: 2 / -1;
   }
 
   button {
-    flex: 25%;
+    grid-column: span 2;
     margin-top: 0.5em;
 
     :hover {
@@ -93,10 +106,10 @@ const Content = styled.div`
   }
 
   .cancel {
-    background-color: darkred;
+    background-color: hsl(359, 75%, 37%);
   }
 
   .save {
-    background-color: var(--color-primary);
+    background-color: var(--color-secondary);
   }
 `
